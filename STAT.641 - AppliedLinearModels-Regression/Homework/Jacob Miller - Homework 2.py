63e4a31da34dc197ee8cce77548e9da8d3cdd77c
Jake <jakemiller13@hotmail.com>
2020-02-05 07:06:48 -0500

    Working on HW 2

--- STAT.641 - AppliedLinearModels-Regression/Homework/Jacob Miller - Homework 2.py
+++ STAT.641 - AppliedLinearModels-Regression/Homework/Jacob Miller - Homework 2.py
@@ -7,7 +7,6 @@ Created on Sat Feb  1 11:17:49 2020
 """
 
 import numpy as np
-import pandas as pd
 from scipy import stats
 
 def title_print(text):
@@ -17,33 +16,67 @@ def title_print(text):
     text_len = len(text)
     print()
     print('#' * (text_len + 4))
-    print('#', text, '#')
+    print('|', text, '|')
     print('#' * (text_len + 4))
 
 #################
 # Class example #
 #################
 title_print('Class example')
-Y = np.array([[166], [163], [151], [177], [163]])
 X = np.array([[12], [12.5], [10], [14], [11]])
 X = np.concatenate((np.ones(X.shape), X), axis = 1)
+Y = np.array([[166], [163], [151], [177], [163]])
 
 inv = np.linalg.inv(np.matmul(X.T, X))
 b_hat = np.matmul(np.matmul(inv, X.T), Y)
 
 X_h = 10.5
-y_hat = b_hat[0] + b_hat[1] * X_h
+y_0 = b_hat[0] + b_hat[1] * X_h
 
 print('b_hat\n', b_hat)
-print('\ny_hat\n', y_hat)
+print('\ny_hat\n', y_0)
+
+#############
+# Problem 1 #
+#############
+X = np.array([[-8], [-4], [0], [4], [8]])
+X = np.concatenate((np.ones(X.shape), X), axis = 1)
+Y = np.array([[11.7], [11], [10.2], [9], [7.8]])
+
+################
+# Calculations #
+################
+# Constants
+alpha = 0.05
+n = len(Y)
+t_stat = stats.t.ppf(alpha / 2, len(Y) - 2)
+x_hat = np.mean(X[:, 1])
+
+# SS_T = sum(y_i**2) - (sum(y_i)**2) / n
+sum_y_squared = sum(Y ** 2)
+squared_sum_y = sum(Y) ** 2
+SS_T = sum_y_squared - squared_sum_y / n
+
+# S_xx = S_xx = sum(x_i**2) - sum(x_i)**2 / n
+sum_xx = sum(X[:, 1] ** 2)
+sum_x_squared_over_n = (sum(X[:, 1])) ** 2 / n
+S_xx = sum_xx - sum_x_squared_over_n
+
+# S_xy = S_xy = sum(y_i * x_i) - sum(y_i) * sum(x_i) / n
+sum_xy = sum(X[:, 1].reshape(-1, 1) * Y)
+sum_x_sum_y_over_n = sum(X[:, 1]) * sum(Y) / n
+S_xy = sum_xy - sum_x_sum_y_over_n
+
+# SS_res = SS_res = SS_T - B_hat[1] * S_xy
+SS_res = SS_T - b_hat[1] * S_xy
+
+# MS_res (variance) = SS_Res / (n-2)
+MS_res = SS_res / (n - 2)
 
 ###############
 # Problem 1.a #
 ###############
 title_print('Problem 1.a')
-X = np.array([[-8], [-4], [0], [4], [8]])
-X = np.concatenate((np.ones(X.shape), X), axis = 1)
-Y = np.array([[11.7], [11], [10.2], [9], [7.8]])
 
 inv = np.linalg.inv(np.matmul(X.T, X))
 b_hat = np.matmul(np.matmul(inv, X.T), Y)
@@ -51,37 +84,43 @@ b_hat = np.matmul(np.matmul(inv, X.T), Y)
 print(b_hat)
 
 ###############
+# Problem 1.b #
+###############
+title_print('Problem 1.b')
+
+var_cov = MS_res * inv
+
+print(var_cov)
+
+###############
 # Problem 1.c #
 ###############
 title_print('Problem 1.c')
-X_h = -6
-y_hat = b_hat[0] + b_hat[1] * X_h
 
-print(y_hat)
+X_0 = -6
+y_0 = b_hat[0] + b_hat[1] * X_0
+
+print(y_0)
 
 ###############
 # Problem 1.e #
 ###############
 title_print('Problem 1.e')
 
-# Constants
-alpha = 0.05
-t_stat = stats.t.ppf(alpha / 2, len(Y) - 2)
+# Prediction interval - note t_stat is negative so ranges are flipped
+parens = 1 + 1/n + (X_0 - x_hat) ** 2 / S_xx
+constant = t_stat * np.sqrt(MS_res * parens)
 
-# Calculate SS_T
-sum_y_squared = sum(Y ** 2)
-squared_sum_y = sum(Y) ** 2
-SS_T = sum_y_squared - squared_sum_y / len(Y)
+y_low = y_0 + constant
+y_high = y_0 - constant
 
-# Calculate S_XY
-sum_xy = sum(X[:,1].reshape(-1, 1) * Y)
-sum_x_sum_y_over_n = sum(X[:, 1]) * sum(Y) / len(Y)
-S_XY = sum_xy - sum_x_sum_y_over_n
+print('-> Prediction Interval <-\n{} <= y_0 <= {}'.format(y_low, y_high))
 
-# Calculate SS_res
-SS_res = SS_T - b_hat[1] * S_XY
+#############
+# Problem 2 #
+#############
+title_print('Problem 2')
 
-# Calculate MS_res (variance)
-MS_res = SS_res / (len(Y) - 2)
+print('Recall b_hat:\n{}'.format(b_hat))
 
-## Using example 2.7 on page 34. 
+print('\n(X`X)^(-1) * X` * E(Y):{}')
