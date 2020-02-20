6f655f9bb4387fb40a3694f3708855ef312fe8b7
Jake <jakemiller13@hotmail.com>
2020-02-20 07:55:34 -0500

    HW3 complete

--- STAT.641 - AppliedLinearModels-Regression/Homework/Jacob Miller - Homework 3.py
+++ STAT.641 - AppliedLinearModels-Regression/Homework/Jacob Miller - Homework 3.py
@@ -12,6 +12,7 @@ import patsy
 import scipy
 import statsmodels.api as sm
 from astropy.table import Table
+from sympy import symbols
 
 def title_print(text):
     '''
@@ -68,7 +69,7 @@ print(results.summary())
 print('\n--- Analysis of Variance table ---\n{}'.format(aov_table))
 print('\nRegression F: {}'.format(round(results.fvalue, 2)))
 print('Regression p: {}'.format(round(results.f_pvalue, 4)))
-print('--> Regression is significant <--')
+print('\n--> Regression is significant <--')
 
 ################
 # Problem 3.1c #
@@ -82,7 +83,7 @@ table = Table([['B2', 'B7', 'B8'], t_values, p_values],
 
 print(table)
 print('\nt-statistic = {}'.format(round(t_stat, 3)))
-print('--> abs(t_0) > t-statistic, so all are significant <--')
+print('\n--> abs(t_0) > t-statistic, so all are significant <--')
 
 ################
 # Problem 3.1d #
@@ -110,7 +111,7 @@ print('reduced y_hat = {} + {} * x_2 + {} * x_8'.format(b_hat2[0],
                                                         b_hat2[1],
                                                         b_hat2[2]))
 print('partial F = {}'.format(partial_F))
-print('--> {} < {}, therefore B7 is significant <--'.format(partial_F,
+print('\n--> {} < {}, therefore B7 is significant <--'.format(partial_F,
       round(results.fvalue, 2)))
 
 ################
@@ -122,7 +123,6 @@ df = pd.read_excel('Data/data-table-B11.xlsx')
 # Problem 3.10a #
 #################
 title_print('Problem 3.10a')
-
 y, X = patsy.dmatrices('Quality ~ Clarity + Aroma + Body + Flavor + \
                        Oakiness', df)
 
@@ -146,7 +146,7 @@ print(results.summary())
 print('\n--- Analysis of Variance table ---\n{}'.format(aov_table))
 print('\nRegression F: {}'.format(round(results.fvalue, 2)))
 print('Regression p: {}'.format(round(results.f_pvalue, 4)))
-print('--> Regression is significant <--')
+print('\n--> Regression is significant <--')
 
 #################
 # Problem 3.10c #
@@ -160,13 +160,12 @@ table = Table([['B1', 'B2', 'B3', 'B4', 'B5'], t_values, p_values],
 
 print(table)
 print('\nt-statistic = {}'.format(round(t_stat, 3)))
-print('--> B4, B5: abs(t_0) > t-statistic so these are significant <--')
+print('\n--> B4, B5: abs(t_0) > t-statistic so these are significant <--')
 
 #################
 # Problem 3.10d #
 #################
 title_print('Problem 3.10d')
-
 y2, X2 = patsy.dmatrices('Quality ~ Aroma + Flavor', df)
 
 parens = np.matmul(X2.T, X2)
@@ -183,3 +182,69 @@ table = Table([['R^2', 'Adj-R^2'],
                names = (' ', 'Full model', 'Reduced model'))
 print(table)
 print('\n--> Very similar, so models are similar <--')
+
+#################
+# Problem 3.10e #
+#################
+title_print('Problem 3.10e')
+ci_1 = np.round(results.conf_int()[4], 3)
+ci_2 = np.round(results2.conf_int()[2], 3)
+
+print('Full model: {} to {}'.format(ci_1[0], ci_1[1]))
+print('Reduced model: {} to {}'.format(ci_2[0], ci_2[1]))
+print('\n--> Very similar again, so similar models <--')
+
+#################
+# Problem 3.25a # Note: this is problem 3.21 in 4th edition
+#################
+title_print('Problem 3.25a')
+b, b0, b1, b2, b3, b4 = symbols('b b0 b1 b2 b3 b4')
+y, x1, x2, x3, x4, eps = symbols('y x1 x2 x3 x4 eps')
+gamma_0, gamma_1, z = symbols('gamma_0 gamma_1 z')
+
+beta = np.array([[b0], [b1], [b2], [b3], [b4]])
+X = np.array([1, x1, x2, x3, x4])
+y = np.matmul(X, beta) + eps
+
+# H0: b1 = b2 = b3 = b4
+beta2 = np.array([[b0], [b1], [b1], [b1], [b1]])
+y2 = np.matmul(X, beta2) + eps
+
+T = np.array([[0, 1, -1, 0, 0],
+              [0, 0, 1, -1, 0],
+              [0, 0, 0, 1, -1]])
+c = np.array([[0], [b], [b], [b], [b]])
+
+print('y = {}'.format(y))
+print('H0: b1 = b2 = b3 = b4 = b')
+print('\nTherefore: b1 - b2 = 0, b2 - b3 = 0, b3 - b4 = 0')
+print('\nT = \n{}\n\nbeta = \n{}\n\nc = \n{}'.format(T, beta, c))
+print('\ny = {}'.format(y2))
+print('\nWhere:\ngamma_0 = b0\ngamma_1 = b\nz = x1 + x2 + x3 + x4')
+print('\n--> Reduced model: y = gamma_0 + gamma_1 * z + eps <--')
+
+#################
+# Problem 3.25b #
+#################
+title_print('Problem 3.25b')
+
+beta = np.array([[b0], [b1], [b2], [b3], [b4]])
+X = np.array([1, x1, x2, x3, x4])
+y = np.matmul(X, beta) + eps
+
+# H0: b1 = b2, b3 = b4
+beta2 = np.array([[b0], [b1], [b1], [b3], [b3]])
+y2 = np.matmul(X, beta2) + eps
+
+T = np.array([[0, 1, -1, 0, 0],
+              [0, 0, 0, 1, -1]])
+c = np.array([[0], [0]])
+
+print('y = {}'.format(y))
+print('H0: b1 = b2, b3 = b4')
+print('\nTherefore: b1 - b2 = 0, b3 - b4 = 0')
+print('\nT = \n{}\n\nbeta = \n{}\n\nc = \n{}'.format(T, beta, c))
+print('\ny = {}'.format(y2))
+print('\nWhere:\ngamma_0 = b0\ngamma_1 = b1\ngamma_3 = b3')
+print('z1 = x1 + x2\nz3 = x3 + x4')
+print('\n--> Reduced model: y = gamma_0 + gamma_1 * z1 + gamma_3 * z3 <--')
