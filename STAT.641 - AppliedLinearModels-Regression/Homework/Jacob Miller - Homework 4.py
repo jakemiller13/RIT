a75c24fca32bb12d08e57e83a1641a97d74f730a
Jake <jakemiller13@hotmail.com>
2020-03-02 07:52:11 -0500

    Working on HW4

--- STAT.641 - AppliedLinearModels-Regression/Homework/Jacob Miller - Homework 4.py
+++ STAT.641 - AppliedLinearModels-Regression/Homework/Jacob Miller - Homework 4.py
@@ -85,8 +85,8 @@ df = pd.DataFrame(data = {'Pressure_(x)': [30, 31, 32, 33, 34, 35, 36],
 # Problem 2.a #
 ###############
 title_print('Problem 2.a')
-df = df[df.columns[0]].append(df[df.columns[1]])
-plt.scatter(x = df.index.values, y = df)
+df_com = df[df.columns[0]].append(df[df.columns[1]])
+plt.scatter(x = df_com.index.values, y = df_com)
 plt.xlabel('Pressure (psi)')
 plt.ylabel('Milage (thousands)')
 plt.show()
@@ -96,15 +96,15 @@ print('--> Optimal tire pressure appears to be between 32 - 34 psi <--')
 # Problem 2.b #
 ###############
 title_print('Problem 2.b')
-y, X = patsy.dmatrices('df.values ~ df.index', df)
+y, X = patsy.dmatrices('df_com.values ~ df_com.index', df_com)
 
 model = sm.OLS(y, X)
 results = model.fit()
 results.model.data.design_info = X.design_info
 
-print('y = {} + {} * x'.format(*np.round((results.params[0],
-                                          results.params[1]),
-                                          2)))
+print('--> y = {} + {} * x <--'.format(*np.round((results.params[0],
+                                                  results.params[1]),
+                                                  2)))
 
 fig, ax = plt.subplots()
 fig = sm.graphics.plot_fit(results, 1, ax = ax)
@@ -116,6 +116,32 @@ plt.show()
 # Problem 2.c #
 ###############
 title_print('Problem 2.c')
+
+# Setup - because each X has exactly 2 y, can just append to dataframe
+df['Mean'] = df.apply(lambda x: np.mean(x), axis = 1)
+df['Sums'] = df.apply(lambda x: (x['Mileage_(y1)'] + x['Mileage_(y2)'] - 
+                                 2 * x['Mean']) ** 2, axis = 1)
+
+# Sum of Squares
+SS_tot = sum(y**2) - sum(y)**2 / len(y)
+SS_reg = np.matmul(np.matmul(np.linalg.inv(np.matmul(X.T, X)), X.T), y)
+# TODO these are betas, need to translate into SS_reg (page 82)
+SS_res = SS_tot - SS_reg
+SS_pe = sum(df['Sums'])
+SS_lof = SS_res - SS_pe
+
+# Degrees of Freedom
+DoF_reg = 1
+DoF_tot = len(df_com) - 1
+DoF_res = DoF_tot - DoF_reg
+DoF_pe = len(df) # because each X has exactly 2 y, this is just num of X's
+DoF_lof = DoF_res - DoF_pe
+
+# Mean Squares
+MS_reg = SS_reg / DoF_reg
+MS_res = SS_res / DoF_res
+MS_pe = SS_pe / DoF_pe
+MS_lof = SS_lof / DoF_lof
 
 ###############
 # Problem 4.3 #
@@ -283,3 +309,35 @@ plt.title('Residuals Versus Predicted Response')
 plt.show()
 
 print('--> Definite non-linear pattern for residual vs. predicted plot <--')
+
+#################
+# Problem 4.17c #
+#################
+title_print('Problem 4.17c')
+
+y2, X2 = patsy.dmatrices('y ~ x2', df)
+model2 = sm.OLS(y2, X2)
+results2 = model2.fit()
+results2.model.data.design_info = X2.design_info
+
+infl1 = results.get_influence()
+infl2 = results2.get_influence()
+press1 = infl1.resid_press
+press2 = infl2.resid_press
+r_squared_pred1 = 1 - \
+    infl1.ess_press/sm.stats.anova_lm(results, typ = 1).sum_sq.sum()
+r_squared_pred2 = 1 - \
+    infl2.ess_press/sm.stats.anova_lm(results2, typ = 1).sum_sq.sum()
+
+comp_press = pd.DataFrame({'Ordinary Residuals': residuals,
+                           'Full Model (PRESS_1)': press1,
+                           'Viscosity to Temp Only (PRESS_2)': press2})
+print(comp_press.to_string())
+
+print('Full Model: PRESS Statistic = {} | R^2 (Pred) = {}%'.\
+      format(round(infl1.ess_press, 3),
+             round(100 * r_squared_pred1, 2)))
+print('Partial Model: PRESS Statistic = {} | R^2 (Pred) = {}%'.\
+      format(round(infl2.ess_press, 3),
+             round(100 * r_squared_pred2, 2)))
+print('--> Full Model: Lower PRESS, higher R^2 implies better prediction <--')
